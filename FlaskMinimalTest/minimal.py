c82d14dc79f2819b391c3d1a1478987200449a6f
ShaneJobes <136496607+ShaneJobes@users.noreply.github.com>
2025-11-17 17:54:39 +0000

    minimal falsk application improved while following along in class

--- FlaskMinimalTest/minimal.py
+++ FlaskMinimalTest/minimal.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1> <p>This Flask application is able to be run!</p>"
+def index():
+    name = "Moon Pie 🐈"
+    return render_template('index.html', name=name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
